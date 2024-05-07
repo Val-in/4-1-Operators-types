01d91583619582ac062b4d70ebcd3f8befb42623
Val-in <valeriia.vp1@yandex.ru>
2024-05-07 22:37:50 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+
+
+   
+    namespace _4._1._Условные_выражения_и_конструкции
+    {
+        internal class Program
+        {
+            public static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+		
+		var color = Console.ReadLine();
+
+		if (color == "red")
+		{
+			Console.BackgroundColor = ConsoleColor.Red;
+			Console.ForegroundColor = ConsoleColor.Black;
+
+			Console.WriteLine("Your color is red!");
+		}
+
+		else if (color == "green")
+		{
+			Console.BackgroundColor = ConsoleColor.Green;
+			Console.ForegroundColor = ConsoleColor.Black;
+
+			Console.WriteLine("Your color is green!");
+		}
+
+        else
+{
+    Console.BackgroundColor = ConsoleColor.Cyan;
+    Console.ForegroundColor = ConsoleColor.Black;
+
+    Console.WriteLine("Your color is cyan!");
+}
+        }
+        }
+}
